e5ea0192520b454b114b0a751dcf3de334472282
Cottonwood <blog_geon@naver.com>
2021-11-02 06:15:00 +0900

    Vue3 webpack template

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2 @click="$emit('click')">
+      {{ msg }}
+    </h2>
+    <img
+      src="~assets/logo.png"
+      alt="HEROPY"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    msg: { type: String }
+  },
+  emits: ['click']
+}
+</script>
+
+<style scoped lang="scss">
+  @import "~/scss/main";
+
+  h2 {
+    font-size: 22px;
+    color: $color--primary;
+    & + img {
+      width: 120px;
+    }
+  }
+</style>
